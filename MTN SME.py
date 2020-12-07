644a871d29d54f3ec901a6dcce9790d5ac10fcc5
Joy Victor <victoriousjvictor@gmail.com>
2020-12-07 17:32:56 +0100

    Updated SME bundle selection with automated OTP

--- MTN SME.py
+++ MTN SME.py
@@ -1,9 +1,14 @@
-#AFTER LOGIN
+
+#from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import ActionChains
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 from webdriver_manager.chrome import ChromeDriverManager
+
+
+# chrome_options = Options()
+# chrome_options.add_argument("--headless")
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -12,19 +17,29 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 def login(number):
     try:
         driver.get("https://mymtn.com.ng/")
-        time.sleep(15)
+        time.sleep(30)
 
         # login
         driver.find_element_by_xpath("//*[@id='mat-input-0']").send_keys(number)
         driver.find_element_by_xpath("//*[@id='label']").click()
-        time.sleep(60)
+        time.sleep(70)
         return "Login Successful"
 
     except:
         print("Incorrect login details or poor internet connection")
 
 
-# OTP Verification would be done manually
+# OTP Verification would be done manually(to be automated)
+def handleOTP():
+    driver.find_element_by_xpath('//*[@id="tat"]/app-login/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div/app-mainbutton').click()
+
+    otp = input("Enter OTP: ")
+    time.sleep(30)
+    driver.find_element_by_xpath('//*[@id="mat-input-3"]').send_keys(otp)
+    time.sleep(0.5)
+    driver.find_element_by_xpath('//*[@id="tat"]/app-login/div/div[2]/div/div/div/div/form/div/div[2]/div/app-mainbutton').click()
+
+
 
 def dashboard():
 
@@ -49,24 +64,33 @@ def subscribe_SME():
     """ Chooses the subscription plan. PLEASE NOTE: THIS SCRIPT ONLY SUBSCRIBES TO 3.5GB FOR #2,500. More
     modification would be done in the future. """
 
-    try:
-        driver.find_element_by_xpath('//*[@id="wht-crv"]/div/div/app-buybundle-submenu/div[1]/div/div/div/a[7]').click()
-        time.sleep(3)
-        driver.find_element_by_xpath('//*[@id="circledata"]').click()
-        time.sleep(5)
+    #try:
+    driver.find_element_by_xpath('//*[@id="wht-crv"]/div/div/app-buybundle-submenu/div[1]/div/div/div/a[7]').click()
+    time.sleep(3)
+    driver.find_element_by_xpath('//*[@id="wht-crv"]/div/div/app-buybundle-submenu/div[2]/app-smebundles/div[2]/div[1]/div[2]').click()
+    time.sleep(10)
 
-        # clicking 3.5GB for 2,500
-        driver.find_element_by_xpath('//*[@id="wht-crv"]/div/div/app-buybundle-submenu/div[2]/app-smebundles/div[1]/div[2]/div[2]/div[3]/app-internalcards/div/div/div/div[2]/div').click()
-        time.sleep(3)
-        driver.find_element_by_xpath('//*[@id="shownxt"]/div/div[2]/app-mainbutton').click()
+    #clicking 500MB
+    driver.find_element_by_xpath('//*[@id="mat-radio-16"]').click()
+    time.sleep(3)
+    driver.find_element_by_xpath('//*[@id="mat-input-9"]').send_keys("08037168316")
+    time.sleep(2)
+    driver.find_element_by_xpath('//*[@id="mat-input-10"]').send_keys("9890")
+    time.sleep(2)
+    driver.find_element_by_xpath('//*[@id="wht-crv"]/div/div/app-buybundle-submenu/div[2]/app-smebundles/div[2]/div[2]/form/div[3]/div[3]/app-mainbutton').click()
 
-        time.sleep(20)
-        driver.close()
+    time.sleep(20)
+        #driver.close()
 
-    except:
-        print("Poor internet connection")
+    # except:
+    #     print("Poor internet connection")
 
 
-login(number="08039506623")
+login(number='07037105968')
+#handleOTP()
 dashboard()
 subscribe_SME()
+
+# '07037105968' Mr KB's number
+
+# 9890 PIN
